a356eb4cea4fa24358de253debf7e0769f66a73e
Cristian Deleon <libaration@gmail.com>
2020-07-13 01:14:59 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,7 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 name_hash = {blake: 500, ashley: 2, adam: 1}
+<<<<<<< HEAD
 
 def key_for_min_value(name_hash)
   min_value = 1000
@@ -20,4 +21,30 @@ end
 
 
 
+=======
+def key_for_min_value(name_hash)
+  needsorting = []
+  name_hash.collect do |name,age| ###iterate through hash
+
+   needsorting << name_hash[name]
+
+  end
+i=0
+  while i < needsorting.size - 1
+    if needsorting[i] > needsorting[i+1]
+        needsorting[i], needsorting[i+1] = needsorting[i+1], needsorting[i]
+
+      end
+    i += 1
+  end
+  for i in needsorting do
+
+    ##print i
+    ##puts name_hash.key(i)
+
+  end
+
+end
+
+>>>>>>> 40dc7e64d6f96898a956f64e63f26fa98f2f1ba4
 key_for_min_value(name_hash)
